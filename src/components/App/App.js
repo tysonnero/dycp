0338a4af6ea9d3b962f9bec3b5c48bc77ef6d6c3
Tyson Nero <tyson.nero@lendingtree.com>
2016-12-08 23:31:51 -0500

    Added Content Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import FlatButton from 'material-ui/FlatButton';
-import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
-import { GridList, GridTile } from 'material-ui/GridList';
 
-import { AppToolbar, SideNav } from '../index';
+import { AppToolbar, Content, SideNav } from '../index';
 
 import './App.css';
 
@@ -24,37 +21,7 @@ class App extends Component {
         <div className="App">
           <AppToolbar onElementLeftClick={this.handleSidebarToggle}/>
           <SideNav docked={docked}/>
-          <div style={{
-            paddingTop: 64,
-            marginLeft: docked ? 256 : 0,
-            transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'
-          }}>
-            <GridList
-              cellHeight={'auto'}
-              cols={1}
-            >
-              <GridTile style={{ padding: 20 }}>
-                <Card initiallyExpanded={true}>
-                  <CardHeader
-                    title="Without Avatar"
-                    subtitle="Subtitle"
-                    actAsExpander={true}
-                    showExpandableButton={true}
-                  />
-                  <CardActions>
-                    <FlatButton label="Action1"/>
-                    <FlatButton label="Action2"/>
-                  </CardActions>
-                  <CardText expandable={true}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-                  </CardText>
-                </Card>
-              </GridTile>
-            </GridList>
-          </div>
+          <Content sideNavDocked={docked}/>
         </div>
       </MuiThemeProvider>
     );
--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,12 @@
 
 import App from './App/App';
 import AppToolbar from './AppToolbar/AppToolbar';
+import Content from './Content/Content';
 import SideNav from './SideNav/SideNav';
 
 export  {
   App,
   AppToolbar,
+  Content,
   SideNav
 }
